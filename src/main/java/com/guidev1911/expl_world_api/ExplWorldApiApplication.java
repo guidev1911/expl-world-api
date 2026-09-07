17f2bbd0529dcaa9ea54c2fc6436fd03b2192211
guidev1911 <guilhermereal1911@gmail.com>
2026-09-07 13:45:10 -0300

    configuração do banco mysql/env/gitignore

---
+++ src/main/java/com/guidev1911/expl_world_api/ExplWorldApiApplication.java
@@ -0,0 +1,13 @@
+package com.guidev1911.expl_world_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExplWorldApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExplWorldApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/guidev1911/expl_world_api/ExplWorldApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guidev1911.expl_world_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExplWorldApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
