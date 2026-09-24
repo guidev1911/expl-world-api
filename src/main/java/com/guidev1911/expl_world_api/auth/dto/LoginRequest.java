d6494c0e25611db6e418c967af8be8b7187e62b7
guidev1911 <guilhermereal1911@gmail.com>
2026-09-23 22:02:58 -0300

    login dtos

---
+++ src/main/java/com/guidev1911/expl_world_api/auth/dto/LoginRequest.java
@@ -0,0 +1,15 @@
+package com.guidev1911.expl_world_api.auth.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public record LoginRequest(
+
+        @NotBlank
+        @Email
+        String email,
+
+        @NotBlank
+        String password
+
+) {}
---
+++ src/main/java/com/guidev1911/expl_world_api/auth/dto/LoginResponse.java
@@ -0,0 +1,5 @@
+package com.guidev1911.expl_world_api.auth.dto;
+
+public record LoginResponse(
+        String token
+) {}
